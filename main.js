63d086556160a821b5b4b143b1733833533b9ee9
sawthedeveloper <sawthedeveloper@gmail.com>
2020-02-11 09:45:00 -0800

    Mobile first design frontend

---
+++ main.js
@@ -0,0 +1,11 @@
+// animate smooth scroll
+$('#view-work').on('click', function(){
+    const images = $('#images').postion().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
